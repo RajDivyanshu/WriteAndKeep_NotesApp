f7aafaa22d430bd6dbbb8c22dd27caba791b7aa1
divyanshu <divy049@gmail.com>
2021-11-29 03:16:19 +0530

    UI part is done

--- app/src/main/java/com/kotlinninja/writeandkeepnotes/ui/fragments/CreateNotesFragment.kt
+++ app/src/main/java/com/kotlinninja/writeandkeepnotes/ui/fragments/CreateNotesFragment.kt
@@ -6,17 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.kotlinninja.writeandkeepnotes.R
+import com.kotlinninja.writeandkeepnotes.databinding.FragmentCreateNotesBinding
 
 
 class CreateNotesFragment : Fragment() {
 
+    lateinit var binding:FragmentCreateNotesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_notes, container, false)
+
+        binding= FragmentCreateNotesBinding.inflate(layoutInflater,container,false)
+        return binding.root
+        //inflater.inflate(R.layout.fragment_create_notes, container, false)
     }
 
 
--- app/src/main/java/com/kotlinninja/writeandkeepnotes/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/kotlinninja/writeandkeepnotes/ui/fragments/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.kotlinninja.writeandkeepnotes.R
 import com.kotlinninja.writeandkeepnotes.databinding.FragmentHomeBinding
 
@@ -12,13 +13,21 @@ import com.kotlinninja.writeandkeepnotes.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
     lateinit var binding: FragmentHomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding= FragmentHomeBinding.inflate(layoutInflater,container,false)
-//returning root
+
+
+        //for this button view is 'it'
+        // navigate from where to where
+        binding.btnAddNotes.setOnClickListener{
+            Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_createNotesFragment)
+        }
+
         return binding.root
     }
 
