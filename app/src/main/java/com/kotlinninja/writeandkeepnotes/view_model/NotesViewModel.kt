a45ef39b1ef32fdc63fcbc7169abec820de02741
divyanshu <divy049@gmail.com>
2021-11-29 13:30:00 +0530

    View Model

---
+++ app/src/main/java/com/kotlinninja/writeandkeepnotes/view_model/NotesViewModel.kt
@@ -0,0 +1,37 @@
+package com.kotlinninja.writeandkeepnotes.view_model
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.kotlinninja.writeandkeepnotes.database.NotesDatabase
+import com.kotlinninja.writeandkeepnotes.model.Notes
+import com.kotlinninja.writeandkeepnotes.repository.NotesRepository
+import kotlinx.coroutines.InternalCoroutinesApi
+
+//androidviewmodel runs in whole application
+@InternalCoroutinesApi
+class NotesViewModel(application: Application):AndroidViewModel(application) {
+
+    val repository:NotesRepository
+
+    init {  // runs it first
+         val dao= NotesDatabase.getDatabaseInstance(application).myNotesDao()
+        repository= NotesRepository(dao)
+
+    }
+
+    fun addNotes(notes: Notes){
+        repository.insertNotes(notes)
+    }
+
+    fun getNotes():LiveData<List<Notes>> = repository.getallNotes()
+
+    fun deleteNotes(id:Int){
+        repository.deleteNotes(id)
+    }
+
+     fun updateNotes(notes: Notes) {
+         repository.updateNotes(notes)
+     }
+
+}
